b83c8ccbfbbb5a8312121e5e6e8ee5bfe6a28834
João Almeida <jsantosalmeida54@gmail.com>
2024-08-17 19:55:48 -0300

    Updates

---
+++ assets/404-CHeynrh2.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-BM6nJJOe.js";const n=()=>o.jsx("div",{children:"404, page not found!"});export{n as default};
---
+++ assets/_id_-DtheL2ug.js
@@ -0,0 +1,1 @@
+import{a as r,j as t}from"./index-BM6nJJOe.js";const o=()=>{const{id:s}=r();return t.jsx("h1",{style:{color:"white"},children:s})};export{o as default};

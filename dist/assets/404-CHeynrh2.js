a42fe68a5836f1ae2872e5b8f08c4e0bce9a10c0
João Almeida <jsantosalmeida54@gmail.com>
2024-08-17 22:07:45 -0300

    bugfix: build

--- dist/assets/404-CHeynrh2.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-BM6nJJOe.js";const n=()=>o.jsx("div",{children:"404, page not found!"});export{n as default};
--- dist/assets/_id_-DtheL2ug.js
+++
@@ -1,1 +0,0 @@
-import{a as r,j as t}from"./index-BM6nJJOe.js";const o=()=>{const{id:s}=r();return t.jsx("h1",{style:{color:"white"},children:s})};export{o as default};
